a96fd92e3533dfbbbca2ddd6fa982861de2a765a
Aashir-897 <aashirahmed510@gmail.com>
2025-07-31 03:50:52 +0500

    minor channes

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
 # index.py
 from flask import Flask, jsonify, request
+import os
 # ... rest of your code
 
 app = Flask(__name__)
 # CORS, routes...
-
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
 # ✅ This line must be present for Vercel to detect `app`
 # (Usually not required but useful if deployment fails)
 app = app
